800dddd04d4f00fcff8a764cf33ea158bd3dd6a7
Valentino12341 <105175770+Valentino12341@users.noreply.github.com>
2025-09-16 16:46:41 +0200

    SCRAMBLEGAME.PY

--- scr.py
+++ scr.py
@@ -8,23 +8,30 @@ win.config(bg="Lightgreen")
 
 
 
-correct = ["apple","banana","cobra","fly","dog","text"]
-jumbled = ["eplpa","anbaan","rocba","lyf","ogd","xtte"]
+correct = [
+    "apple", "banana", "cobra", "fly", "dog", "text", "python", "random", "jungle", "wizard", "orange", "planet","school", "friend", "cloud", "window", "travel", "garden", "bridge", "dream", "storm", "castle", "button", "mirror","forest", "rocket", "dragon", "island", "secret", "silver", "giants", "camera", "pirate", "helmet", "tunnel", "puzzle","clocks", "energy", "hunter", "object", "spaces", "danger", "magic", "future", "glider", "goblin", "desert", "ocean","rhythm", "crystal", "potion", "flames", "marvel", "quest", "robot", "cyborg", "dancer", "melody", "singer", "artist","vision", "shapes", "number", "plasma", "galaxy", "vector", "bounce", "clover", "rabbit", "bubble", "gadget", "circle","square", "pillow", "feather", "pencil", "candle", "laptop", "shadow", "spaceship", "gravity", "science", "pyramid","oxygen", "matrix", "cloudy", "bridge", "castle", "window", "planet", "travel", "mirror", "orange", "python", "jungle","banana", "planet", "wizard", "garden", "random", "storm", "dream", "friend", "school"
+]
+
+jumbled = [     
+    "eplpa", "anbaan", "rocba", "lyf", "ogd", "xtte", "typhon", "omdnar", "ugnlej", "dazwir", "aegorn", "nelpat","cohlos", "nerdif", "ludco", "ndwoiw", "elvrat", "edgnar", "igrdeb", "amrde", "ormts", "tslcae", "tutobn", "rorrim","oerstf", "kctoer", "gonadr", "danlsi", "ceerst", "vresil", "tsiang", "armeca", "atrepi", "temhel", "nelnut", "elupzz","skcolc", "gnyree", "nteruh", "jecobt", "scespa", "rdegan", "cigam", "rueftu", "dirgel", "nlogib", "tsreed", "cnaeo","myhtrh", "tscryal", "tionpo", "sfelma", "mlvear", "etqsu", "obotr", "bgryco", "rcedna", "yodlem", "rgenis", "tsitra","sionvi", "hspase", "rebmun", "slpmaa", "laxgay", "cetrov", "ceubon", "velocr", "tibbar", "blubbe", "tedgag", "lrecic","rauqes", "wollip", "aretfeh", "licpen", "ladcen", "ptolpa", "odswah", "ipchseass", "tivgary", "cenecsi", "darypmi","noyxge", "ratimx", "dculoy", "edbirg", "lceast", "wonidw", "nelpta", "elvart", "rorrim", "agroen", "typhon", "genjul","nanaba", "tnleap", "dazwri", "nedgra", "nordam", "mtsor", "meadr", "rdenfi", "holcos"
+]
 storecor = ""
 storejumb = ""
 score = 0
 attempt = -1
 penatly = 0
 first_hint = 0
-
+hintindex = 1
 
 def hint():# HINT BUTTON
-    global storecor,storejumb,penatly,attempt,first_hint
-    print(storecor[0])
+    global storecor,storejumb,penatly,attempt,first_hint,hintindex
+    print(storecor[hintindex])
     first_hint +=1
-    hint_label.config(text="the first letter ="+storecor[0])
+    
+    hint_label.config(text="the first letter ="+storecor[0:hintindex])
     penatly += 1
     penatly_label.config(text="penalty for hint = "+str(penatly))
+    hintindex +=1
     if penatly == 3:
         attempt += 1
         penatly = 0
@@ -34,16 +41,16 @@ def hint():# HINT BUTTON
     if first_hint == 1:
         messagebox.showinfo("hints","3 hints = 1 try ")
 
-
-
 def newword(): #RESET BUTTON
-    global correct,jumbled,storecor,storejumb,attempt
-    ra = random.randint(0,5)
+    global correct,jumbled,storecor,storejumb,attempt,hintindex
+    hintindex = 1
+    ra = random.randint(0,101)
     storecor = correct[ra]
     storejumb = jumbled[ra]
     qL.config(text=storejumb)
     attempt += 1
     sL.config(text=str(score)+"/"+str(attempt))
+    hint_label.config(text="")
 
 def check(): #CHECK BUTTON
     global correct,jumbled,storecor,storejumb,score,attempt
